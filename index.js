3d682c52988f2de29bbfdbbee003b3fdbc4d20a9
DaveDeSmoker <60197890+DaveDeSmoker@users.noreply.github.com>
2020-02-18 10:25:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -174,12 +174,13 @@ bot.on("message", async message => {
         return;
 
     }
-        if(cmd === `${prefix}dm`){      
+    if(cmd === `${prefix}dm`){      
             
         var dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+        var dMessage = args.join(" ").slice(22);
+        
         if (!dUser) return message.channel.send("Can't find user!")
         if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't you that command!")
-        let dMessage = args.join(" ").slice(22);
         if(dMessage.length < 1) return message.reply('You must supply a message!')
 
         dUser.send(`${dUser} A moderator from WP Coding Club sent you: ${dMessage}`)
