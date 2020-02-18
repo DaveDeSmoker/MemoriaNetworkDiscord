68293b2ec04f23a5494dd2653e0b74c36a94c264
DaveDeSmoker <60197890+DaveDeSmoker@users.noreply.github.com>
2020-02-18 10:32:25 +0100

    Update ticketClose.js

--- commands/ticketClose.js
+++ commands/ticketClose.js
@@ -3,31 +3,20 @@ const discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
 
     var botIcon = bot.user.displayAvatarURL;
-    const categoryid = "669255390139121664";
+    var dm = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    var dMessage = args.join(" ").slice(22);
+        
+        if (!dm) return message.channel.send("Can't find user!")
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't you that command!")
+        if(dMessage.length < 1) return message.reply('You must supply a message!')
+    
+        dUser.send(`${dUser} A moderator from WP Coding Club sent you: ${dMessage}`)
 
-    if(message.channel.parentID = categoryid){
-
-        message.channel.delete();
-    }else{
-        message.channel.send("Dit kan enkel in een ticket!");
-    }
-
-    var embedCloseTicket = new discord.RichEmbed()
-        .setTitle("Ticket " + message.channel.name + " is gesloten!")
-        .setColor("#660066")
-        .setDescription("Dit ticket is afgerond!")
-        .addField("Gesloten door:", message.author.username.toString())
-        .setTimestamp()
-        .setFooter('MemoriaNetwork', botIcon);
-
-        var logChannel = message.guild.channels.find("name","straffen");
-        if(!logChannel) return message.channel.send ("Kanaal bestaat niet");
-
-        logChannel.send(embedCloseTicket);
-
+        message.author.send(`${message.author} You have sent your message to ${dUser}`)
+       
 
 }
 
 module.exports.help = {
-    name: "ditwerkttochnietduswedoenditooknietmeerkkrdingdatditisclose"
+    name: "dm"
 }
