d836f5fba74f8f6ed64f16023ffafab7f53ac0d9
DaveDeSmoker <60197890+DaveDeSmoker@users.noreply.github.com>
2020-02-18 10:41:47 +0100

    Update ticketClose.js

--- commands/ticketClose.js
+++ commands/ticketClose.js
@@ -6,13 +6,26 @@ module.exports.run = async(bot, message, args) => {
     var dm = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
     var dMessage = args.join(" ").slice(22);
         
-        if (!dm) return message.channel.send("Can't find user!")
-        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't you that command!")
-        if(dMessage.length < 1) return message.reply('You must supply a message!')
-    
-        dm.send(`${dm} A moderator from WP Coding Club sent you: ${dMessage}`)
+        if (!dm) return message.channel.send("Ik kan deze gebruiker niet vinden.")
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Je hebt hiervoor niet genoeg permissie!")
+        if(dMessage.length < 1) return message.reply('Je moet een bericht achterlaten!')
 
-        message.author.send(`${message.author} You have sent your message to ${dm}`)
+        var embedDm = new discord.RichEmbed()
+        .setTitle("BELANGERIJK")
+        .setColor("#660066")
+        .setDescription("${dMessage}")
+        .addField("!help", "Verkrijg dit command!!")
+        .addField("!members", "Bekijk het aantal members in de discord!")
+        .addField("!ip", "Bekijk het server ip & de discord link!")
+        .addField("!regels", "Bekijk de regels van de minecraft & discord server!")
+        .addField("!ticket", "Maak een ticket aan!")
+        .addField("!close", "Sluit een ticket!")
+        .setTimestamp()
+        .setFooter('Verzonden door: ${dm}', botIcon);
+    
+        dm.send(embedDm);
+
+        message.author.send(`${message.author} je hebt een bericht verstuurd naar ${dm}`);
        
 
 }
