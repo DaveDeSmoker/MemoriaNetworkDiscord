0a9a72af518c4d1a222225e2e3fef4e5a5f5e197
DaveDeSmoker <60197890+DaveDeSmoker@users.noreply.github.com>
2020-02-22 20:46:05 +0100

    Create calc.js

---
+++ commands/calc.js
@@ -0,0 +1,22 @@
+const math = require('mathjs');
+const Discord = require('discord.js');
+
+exports.run = (client, message, args, tools) => {
+
+    if (!args[0]) return message.channel.send("Zet er een som bij!");
+    
+    let resp;
+    try {
+        resp = math.eval(args.join(" "));
+    } catch (e) {
+         return message.channel.send("Zet er een mogelijke rekensom bij");
+    }
+    
+    const embed = new Discord.MessageEmbed()
+        .setColor(0xfffff)
+        .setTitle("REKENMACHINE)
+        .setDescription(`\`\`\`js\n${args.join(" ")}\`\`\``)
+        .addField("Uitkomst", ´\´\´\´js\n${resp}\`\`\``)
+     message.channel.send(embed);
+     
+}
