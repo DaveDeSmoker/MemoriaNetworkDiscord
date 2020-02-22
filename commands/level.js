8d0393d8468ff57f8df98d22d2d7031e8a820f3a
DaveDeSmoker <60197890+DaveDeSmoker@users.noreply.github.com>
2020-02-22 19:03:50 +0100

    Update level.js

--- commands/level.js
+++ commands/level.js
@@ -1,4 +1,4 @@
-const fileLevels = require("./level.json");
+const fileLevels = require("../level.json");
 const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
