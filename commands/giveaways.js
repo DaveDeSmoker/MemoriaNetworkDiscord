cb373e4a85176e16b0b4d8e2606e484fa9565f1d
DaveDeSmoker <60197890+DaveDeSmoker@users.noreply.github.com>
2020-02-22 20:05:54 +0100

    Update giveaways.js

--- commands/giveaways.js
+++ commands/giveaways.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
     var time;
     var winnerCount;
     var d = new Date,
-    dformat = [d.getMonth()+1,d.getDate(),[d.getHours(),d.getMinutes()].join(':');
+    dformat = [d.getMonth()+1,d.getDate(),[d.getHours(),d.getMinutes()];
                
  
     // Nakijken als je perms hebt om dit command te doen.
