1ece8bc8820788524931162783289c764b9cf030
DaveDeSmoker <60197890+DaveDeSmoker@users.noreply.github.com>
2020-03-16 00:53:43 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
         var embedHelp = new discord.RichEmbed()
         .setTitle("__**HELP**__")
         .setColor("#660066")
-        .setDescription("\n\n__**INFORMATION!**__\n**!help** | *Get this command*\n**!members** | *View the number of members in the discord*\n**!ip** | *View the server ip & the discord link*\n**!rules** | *View the rules of the minecraft & discord server*\n**!info** | *Get info about the discord bot*\n\n__**FEEDBACK**__\n**!idee {idea}** | *Leave an idea*\n**!review {number} {review}** | *Leave a review*\n\n__**GAMES**__\n**!sps {stone} {paper} {shear}** | *Play rock paper scissors*\n\n__**TICKET**__\n**!new** | *Create a ticket*")
+        .setDescription("\n\n__**INFORMATION!**__\n**!help** | *Get this command*\n**!members** | *View the number of members in the discord*\n**!ip** | *View the server ip & the discord link*\n**!rules** | *View the rules of the minecraft & discord server*\n**!info** | *Get info about the discord bot*\n\n__**FEEDBACK**__\n**!idee {idea}** | *Leave an idea*\n**!review {number} {review}** | *Leave a review*\n\n__**GAMES**__\n**!sps {stone} {paper} {shear}** | *Play rock paper scissors*\n\n__**TICKET**__\n**!new** | *Create a ticket*\n**!close** | *Close a ticket*")
         .setTimestamp()
         .setFooter('MemoriaNetwork', botIcon);
 
