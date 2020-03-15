3f19ec0dac90b88777770171258fe6055b07989a
DaveDeSmoker <60197890+DaveDeSmoker@users.noreply.github.com>
2020-03-16 00:30:29 +0100

    Update announcement.js

--- commands/announcement.js
+++ commands/announcement.js
@@ -3,16 +3,16 @@ const discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
     var botIcon = bot.user.displayAvatarURL;
 
-    if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("Je hebt helaas niet de juiste permissie!");
+    if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("You dont have the permissions for that!");
 
     var splitser = "//";
 
         if(args[0] == null) {
 
             var useMessage = new discord.RichEmbed()
-            .setTitle("GEBRUIK ANNOUNCEMENT")
+            .setTitle("USE ANNOUNCEMENT")
             .setColor("#660066")
-            .setDescription(`Maak een announcement door gebruik te maken van: \n !announcement Bericht ${splitser} Kleur ${splitser} Kanaal`)
+            .setDescription(`Make an announcement using: \n !announcement message ${splitser} color ${splitser} channel name ${splitser} title`)
             .setTimestamp()
             .setFooter('MemoriaNetwork', botIcon);
 
@@ -23,13 +23,15 @@ module.exports.run = async(bot, message, args) => {
         args = args.join(" ").split(splitser);
 
         if(args[1] == undefined) args[1] = "#660066";
-        if(args[2] == undefined) args[2] = "meldingen";
+        if(args[2] == undefined) args[2] = "announcements";
+        if(args[3] == undefined) args[3] = "Announcement";
 
         var options = {
 
-            bericht: args[0] ||"Geen inhoud opgegeven",
+            bericht: args[0] ||"NO INFORMATION",
             kleur: args[1].trim(),
-            kanaal: args[2].trim()
+            kanaal: args[2].trim(),
+            naam: args[3].trim()
 
 
         }
@@ -37,14 +39,14 @@ module.exports.run = async(bot, message, args) => {
         var announcer = message.author;
 
         var announcementEmbed = new discord.RichEmbed()
-        .setTitle("Announcement")
+        .setTitle(` ${options.naam} `)
         .setColor(` ${options.kleur} `)
         .setDescription(`\n ${options.bericht} \n`)
         .setTimestamp()
         .setFooter(`MemoriaNetwork`, botIcon);
 
     var announcementChannel = message.guild.channels.find(`name`, options.kanaal);
-    if(!announcementChannel) return message.channel.send("Kan het kanaal niet vinden!");
+    if(!announcementChannel) return message.channel.send("Cannot find the channel!");
 
     announcementChannel.send(announcementEmbed);
 
