17376eaaaa5e8d3a7d9be25c5baaa096f0d2cb45
DaveDeSmoker <60197890+DaveDeSmoker@users.noreply.github.com>
2020-03-16 01:12:36 +0100

    Update staffhelp.js

--- commands/staffhelp.js
+++ commands/staffhelp.js
@@ -8,24 +8,13 @@ module.exports.run = async (bot, message, args) => {
         var embedHelp = new discord.RichEmbed()
         .setTitle("STAFF HELP")
         .setColor("#660066")
-        .setDescription("Bekijk alle staff commands van de discord!")
-        .addField("**!announcement {bericht}//{colorcode}//{kanaal}ZONDER #**", "Maak een melding in de discord!")
-        .addField("**!dm {user} {bericht}**", "Verstuur een DM met deze bot (Gewone tekst)!")
-        .addField("**!dmembed {user} {bericht}**", "Verstuur een DM met deze bot (Speciale tekst)")
-        .addField("**!ban {user} {reden}**", "Verban een user van de discord!")
-        .addField("**!tempban {user} {tijd} {reden}**", "Verban een user tijdelijk van de discord!")
-        .addField("**!mute {user}**", "Mute een persoon in de discord!")
-        .addField("**!tempmute {user} {tijd} {reden}**", "Mute een persoon tijdelijk in de discord!")
-        .addField("**!kick {user}**", "Kick een user van de discord server!")
-        .addField("**!clear {aantal}**", "Verwijder een aantal berichten in een channel!")
-        .addField("**!close**", "Sluit een ticket!")
-        .addField("**!rename {naam}**", "Hernoem een ticket channel!")
+        .setDescription("__**PUNISHMENT**__\n**!ban {user} {reden}** | *Ban a user*\n**!tempban {user} {tijd} {reden}** | *Tempban a user*\n**!mute {user}** | *Mute a person*\n**!tempmute {user} {tijd} {reden}** | *Tempmute a user*\n**!kick {user}** | *Kick a user*\n\n__**TICKET**__\n**!close** | *Close a ticket*\n**!rename {name}** | *Rename & ticket*\n\n__**OTHERS**__\n**!announcement {bericht}//{colorcode}//{kanaal}ZONDER #//title** | *Create a announcement*\n**!clear {number}** | *Delete some messages in a channel!*")
         .setTimestamp()
         .setFooter('MemoriaNetwork', botIcon);
 
         message.author.send(embedHelp);
 
-    message.reply("Bekijk je prive berichten voor de commands!").then(message => message.delete(10000));
+    message.reply("View your private messages for the commands!").then(message => message.delete(10000));
 
     }catch (error) {
         message.channel.send("Er is iets fout gegaan!");
